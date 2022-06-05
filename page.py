d66a0bddc9b67564c4e32d0a62c5a840cd556765
najdi013@gmail.com <najdi013@gmail.com>
2022-06-05 17:24:33 +0200

    first steps

---
+++ page.py
@@ -0,0 +1,14 @@
+from element import BaseElement
+
+
+class BasePage(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+
+class MainPage(BasePage):
+    pass
+
+
+class ContactUsPage(BasePage):
+    pass
