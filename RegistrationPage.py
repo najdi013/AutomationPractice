aecc1decc981fc99171d45e05ee1273d68b54745
najdi013@gmail.com <najdi013@gmail.com>
2022-06-20 22:27:45 +0200

    fix EC tuple

--- RegistrationPage.py
+++ RegistrationPage.py
@@ -2,14 +2,15 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#from locators.auto_locators import SignInLocators as SIL
+from locators.auto_locators import SignInLocators as SIL
 from locators.auto_locators import MainPageLocators as MPL
 
 
-class Create(unittest.TestCase):
+class CreateAccount(unittest.TestCase):
     """Test of a creating an account"""
 
     def setUp(self):
+        """Go into Sign In website"""
         self.driver = webdriver.Chrome(
             'C:\Program Files (x86)\Chromedriver.exe')
         self.driver.get(
@@ -19,7 +20,11 @@ class Create(unittest.TestCase):
         self.driver.close()
 
     def test_creat_account(self):
-        self.driver.find_element(*MPL.CONTACT_US).click()
+        """Input data & create account."""
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(MPL.SIGN_IN)).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(SIL.EMAIL)).sendKeys('mail')
 
 
 if __name__ == '__main__':
